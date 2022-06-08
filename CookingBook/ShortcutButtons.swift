691516b48f27e5696b9e46e157c5844431a61bb2
Roland Schmitz <rola@2rs.de>
2022-06-08 07:45:16 +0200

    Hierarchical button layout

--- CookingBook/ShortcutButtons.swift
+++ CookingBook/ShortcutButtons.swift
@@ -14,43 +14,57 @@ struct ShortcutButtons: View {
 
     var body: some View {
         VStack {
-            HStack {
-                Button("Starters") {
-                    print("Starters")
-                    categorySelection = "Starters"
-                    recipeSelection = nil
-                }
-                Group {
-                    Button("Bruschetta") {
-                        print("Bruschetta")
-                        categorySelection = "Starters"
-                        recipeSelection = "Bruschetta"
-                    }
-                    Button("Caprese") {
-                        print("Caprese")
-                        categorySelection = "Starters"
-                        recipeSelection = "Mozzarella Caprese"
-                    }
-                }.padding(.leading)
+            Button("Categories") {
+                print("Categories")
+                categorySelection = nil
+                recipeSelection = nil
             }
+            .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
             HStack {
-                Button("Main Dishes") {
-                    print("Main Dishes")
-                    categorySelection = "Main Dishes"
-                    recipeSelection = nil
+                VStack {
+                    Button("Starters") {
+                        print("Starters")
+                        recipeSelection = nil
+                        // categorySelection = "Starters"
+                    }
+                    .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
+                    HStack {
+                        Button("Bruschetta") {
+                            print("Bruschetta")
+                            categorySelection = "Starters"
+                            recipeSelection = "Bruschetta"
+                        }
+                        .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
+                        Button("Caprese") {
+                            print("Caprese")
+                            categorySelection = "Starters"
+                            recipeSelection = "Mozzarella Caprese"
+                        }
+                        .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
+                    }
                 }
-                Group {
-                    Button("Carbonara") {
-                        print("Carbonara")
+                VStack {
+                    Button("Main Dishes") {
+                        print("Main Dishes")
                         categorySelection = "Main Dishes"
-                        recipeSelection = "Spaghetti Carbonara"
+                        recipeSelection = nil
                     }
-                    Button("Bolognese") {
-                        print("Bolognese")
-                        categorySelection = "Main Dishes"
-                        recipeSelection = "Ragu alla Bolognese"
+                    .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
+                    HStack {
+                        Button("Carbonara") {
+                            print("Carbonara")
+                            categorySelection = "Main Dishes"
+                            recipeSelection = "Spaghetti Carbonara"
+                        }
+                        .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
+                        Button("Bolognese") {
+                            print("Bolognese")
+                            categorySelection = "Main Dishes"
+                            recipeSelection = "Ragu alla Bolognese"
+                        }
+                        .padding(5).background(Color(.systemGray6)).cornerRadius(10).padding(5)
                     }
-                }.padding(.leading)
+                }
             }
         }
     }
